8f62a15f71cde06d681b742966e97ac6855ebcff
verah <verahnyagaka101@gmail.com>
2022-08-16 22:57:53 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count
+    
+    def initialize(title)
+        @title = title
+    end
+    
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+baby = Book.new('sassy')
+puts baby.turn_page
+puts baby.title
 
 
